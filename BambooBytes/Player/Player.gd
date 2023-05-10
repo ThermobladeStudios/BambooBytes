c8748ea34e6540c407302218a5b9831aaccadf56
Alex He <donut11one@gmail.com>
2023-05-10 13:17:32 -0700

    Player Changes

--- BambooBytes/Player/Player.gd
+++ BambooBytes/Player/Player.gd
@@ -5,6 +5,6 @@ extends CharacterBody2D
 
 func _process(delta):
 	var input_direction = Vector2(Input.get_action_strength("Right") - Input.get_action_strength("Left") , Input.get_action_strength("Down") - Input.get_action_strength("Up"))
-	
+
 	velocity = input_direction * speed
 	move_and_slide()
