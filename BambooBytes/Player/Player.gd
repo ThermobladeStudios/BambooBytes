8cdcb5f1fda84cf23dedef96c8320473fcb80ba5
Mentholantum <harrisontseng7@gmail.com>
2023-05-10 13:38:38 -0700

    Added sprint to testGame
    PRESS SHIFT TO MOVE PURPLE BLOCK SLIGHTLY FASTER

--- BambooBytes/Player/Player.gd
+++ BambooBytes/Player/Player.gd
@@ -5,6 +5,12 @@ extends CharacterBody2D
 
 func _process(delta):
 	var input_direction = Vector2(Input.get_action_strength("Right") - Input.get_action_strength("Left") , Input.get_action_strength("Down") - Input.get_action_strength("Up"))
-
+	if(Input.is_action_pressed("Sprint")):
+		speed=1000
+	elif(Input.is_action_just_released):
+		speed=500
 	velocity = input_direction * speed
 	move_and_slide()
+
+
+	
