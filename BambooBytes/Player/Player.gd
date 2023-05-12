c6edf4ae40d68301ada3a7d441b2a02405133b05
Thermoblade Studios <thermobladestudios@gmail.com>
2023-05-12 14:39:03 -0700

    Added GUI and updated bamboo
    - Added a bamboo counter (Still needs to be programmed)
    - Updated bamboo progress bar
    - Created a theme for all GUI elements
    - Fixed positioning of nodes for bamboo
    - Increased windows size
    - Set game to run fullscreen

--- BambooBytes/Player/Player.gd
+++ BambooBytes/Player/Player.gd
@@ -5,12 +5,10 @@ extends CharacterBody2D
 
 var strength = 5
 
-func _process(delta):
+func _physics_process(delta):
 	var input_direction = Vector2(Input.get_action_strength("Right") - Input.get_action_strength("Left") , Input.get_action_strength("Down") - Input.get_action_strength("Up"))
 	if(input_direction != Vector2.ZERO):
 		collision.position = input_direction * 125
-	
-	
 	
 	if(Input.is_action_pressed("Sprint")):
 		speed=1000
