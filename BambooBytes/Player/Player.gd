d97f097f3b391fae628c29f11676ad5bb990b414
Thermoblade Studios <thermobladestudios@gmail.com>
2023-05-15 18:07:20 -0700

    Added dashing and fixed movement
    - Added dashing
    - Removed running as I felt like it would be redundant since most players would always be run
    - Normalized movement so moving diagonally would be the same speed as left, right, up and down

--- BambooBytes/Player/Player.gd
+++ BambooBytes/Player/Player.gd
@@ -2,21 +2,34 @@ extends CharacterBody2D
 
 @onready var collision = $Chop_area
 @export var speed = 500
+@export var DashTime = 0.15
+@export var DashMultiplier = 5
 
 var strength = 5
+var Dashing = false
+var DashCooldown = false
 
 func _physics_process(delta):
-	var input_direction = Vector2(Input.get_action_strength("Right") - Input.get_action_strength("Left") , Input.get_action_strength("Down") - Input.get_action_strength("Up"))
-	if(input_direction != Vector2.ZERO):
-		collision.position = input_direction * 125
 	
-	if(Input.is_action_pressed("Sprint")):
-		speed=1000
+	var input_direction = Vector2(Input.get_action_strength("Right") - Input.get_action_strength("Left") , Input.get_action_strength("Down") - Input.get_action_strength("Up")).normalized()
+	
+	if input_direction != Vector2.ZERO and !Dashing:
+		collision.position = input_direction * 125
+		
+	if Input.is_action_pressed("Dash") and !DashCooldown:
+		Dashing = true
+		DashCooldown = true
+		$Timer.start(0.15)
+		$DashCooldown.start(1)
+		
+	if Dashing:
+		input_direction *= DashMultiplier
 
-	elif(Input.is_action_just_released):
-		speed=500
 	velocity = input_direction * speed
 	move_and_slide()
 
+func _on_timer_timeout():
+	Dashing = false
 
-	
+func _on_dash_cooldown_timeout():
+	DashCooldown = false
