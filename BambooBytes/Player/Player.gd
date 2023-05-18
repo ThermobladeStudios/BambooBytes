4b7ac6aa79e68bd7dd4bafc3ca76606de75e46ac
Alex He <donut11one@gmail.com>
2023-05-18 14:22:52 -0700

    Forgor to add these changes

--- BambooBytes/Player/Player.gd
+++ BambooBytes/Player/Player.gd
@@ -13,15 +13,17 @@ var Dashing = false
 var DashCooldown = false
 var DashDirection
 
-func _ready():
-	collisionShape.transforms
 
 func _physics_process(delta):
 	
 	var input_direction = Vector2(Input.get_action_strength("Right") - Input.get_action_strength("Left") , Input.get_action_strength("Down") - Input.get_action_strength("Up")).normalized()
-	
+	if(Input.is_action_just_pressed("Debugadd")):
+		collisionShape.shape.size += Vector2(10,10)
+	if(Input.is_action_just_pressed("Debugsubtract")):
+		collisionShape.shape.size -= Vector2(10,10)
+		
 	if input_direction != Vector2.ZERO and !Dashing:
-		collision.position = input_direction * 125
+		collision.position = input_direction*($CollisionShape2D.shape.size)/2+ input_direction*(collisionShape.shape.size/2)
 		
 	if Input.is_action_pressed("Dash") and !DashCooldown and input_direction != Vector2.ZERO:
 		Dashing = true
--- BambooBytes/World Objects/bamboo.gd
+++ BambooBytes/World Objects/bamboo.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 var totalTime = 20
 var currTime
 var ischopping = false
-var x = 0
+var x = false
 @export var DroppedAmmount : int
 
 @export var regrowRate = 5
@@ -39,28 +39,24 @@ func _process(delta):
 		currTime = growthbar
 		tree_is_chopped = true
 		regrow.start()
-
-
+	if(x):
+		if(Input.is_action_just_pressed("Attack")):
+			if(tree_is_chopped == false):
+				currTime -= Player.strength
+				print(currTime)
+				var tween = get_tree().create_tween()
+				tween.tween_property(bar, "value", currTime, 0.4).set_trans(Tween.TRANS_QUAD)
 func _on_area_2d_area_entered(body):
-	x+=1
-	if(x != 0):
-		timer.start()
+	x= true
 
+	
 
 func _on_area_2d_area_exited(body):
-	timer.stop()
-	x-=1
+	x=0
 
 func upgrade_drop():
 	DroppedAmmount +=1
 
-
-func _on_timer_timeout():
-	if(tree_is_chopped == false):
-		currTime -= Player.strength*(x)
-		print(currTime)
-		var tween = get_tree().create_tween()
-		tween.tween_property(bar, "value", currTime, 0.4).set_trans(Tween.TRANS_LINEAR)
 
 
 func _on_regrow_timeout():
