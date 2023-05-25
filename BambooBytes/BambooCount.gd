2024ec12adf79db1dda3f3ef4ef485994fc64724
Thermoblade Studios <thermobladestudios@gmail.com>
2023-05-25 03:03:13 -0700

    Save Bamboo Count
    - Added saving bamboo count

--- BambooBytes/BambooCount.gd
+++ BambooBytes/BambooCount.gd
@@ -1,9 +1,13 @@
 extends Label
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+var config = ConfigFile.new()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	if config.load("user://BambooBytes.cfg") == OK:
+		PInven.bamboocount = config.get_value("Currency", "BambooCount")
+
 func _process(delta):
+	var BambooCount = PInven.bamboocount
 	text = str(PInven.bamboocount)
+	config.set_value("Currency", "BambooCount", BambooCount)
+	config.save("user://BambooBytes.cfg")
